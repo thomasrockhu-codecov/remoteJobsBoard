0d451b66db63cf71cb63701d04a9a2d21e2ecc01
Dmitry Kuznetsov <dsk1306@icloud.com>
2021-09-19 03:16:32 +0300

    Cache API responses (#34)

--- RemoteJobsBoard/Extensions/Publisher+Ext.swift
+++ RemoteJobsBoard/Extensions/Publisher+Ext.swift
@@ -3,6 +3,13 @@ import Foundation
 
 public extension Publisher {
 
+    /// Performs the specified closures when publisher receives a value from the upstream publisher.
+    /// - Parameter handler: A closure that executes when the publisher receives a value from the upstream publisher.
+    /// - Returns: A publisher that performs the specified closures when publisher events occur.
+    func handleOutput(_ handler: @escaping (Output) -> Void) -> Publishers.HandleEvents<Self> {
+        handleEvents(receiveOutput: handler)
+    }
+
     /// Attaches a subscriber with closure-based behavior.
     /// - Parameter receiveValue: The closure to execute on completion.
     func sink(receiveValue: @escaping ((Output) -> Void)) -> AnyCancellable {
--- RemoteJobsBoard/Services/API/APIService.swift
+++ RemoteJobsBoard/Services/API/APIService.swift
@@ -3,10 +3,17 @@ import Foundation
 
 final class APIService {
 
+    // MARK: - Typealiases
+
+    fileprivate typealias Output = URLSession.DataTaskPublisher.Output
+    fileprivate typealias DataTaskPublisher = AnyPublisher<Output, ServiceError>
+
     // MARK: - Properties
 
-    private let baseURL = URL(string: "https://remotive.io/api")
     private let requestQueue = DispatchQueue(label: "APIReqiestQueue", qos: .userInitiated)
+
+    private let session = makeSession()
+    private var urlCache: URLCache? { session.configuration.urlCache }
 
 }
 
@@ -14,24 +21,98 @@ final class APIService {
 
 private extension APIService {
 
-    static func decode<Model: Decodable>(type: Model.Type, from data: Data) -> AnyPublisher<Model, ServiceError> {
-        Just(data)
-            .tryMap { try JSONDecoder().decode(Model.self, from: $0) }
-            .mapError { ServiceError.decoding($0) }
-            .eraseToAnyPublisher()
+    static func makeSession() -> URLSession {
+        let configuration = URLSessionConfiguration.default
+
+        let directory = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask)
+            .first?
+            .appendingPathComponent(Constant.responsesCachePath)
+        configuration.urlCache = URLCache(
+            memoryCapacity: Constant.capacity,
+            diskCapacity: Constant.capacity,
+            directory: directory
+        )
+
+        return URLSession(configuration: configuration)
+    }
+
+    static func decode<Model: Decodable>(type: Model.Type, from data: Data) throws -> Model {
+        do {
+            return try JSONDecoder().decode(Model.self, from: data)
+        } catch {
+            throw ServiceError.decoding(model: Model.self, error: error)
+        }
+    }
+
+    static func validate(output: Output) throws -> Output {
+        switch (output.response as? HTTPURLResponse)?.statusCode {
+        case 200:
+            return output
+        case 429:
+            throw ServiceError.tooManyRequests
+        default:
+            throw ServiceError.badServerResponse
+        }
+    }
+
+    func cachedResponse(for request: URLRequest, maxAge: TimeInterval? = nil) -> CachedURLResponse? {
+        guard let cachedResponse = urlCache?.cachedResponse(for: request) else { return nil }
+
+        if let maxAge = maxAge {
+            let currentDate = Date()
+            guard
+                let date = cachedResponse.userInfo?[Constant.cacheDateKey] as? Date,
+                date.distance(to: currentDate) > 0,
+                date.distance(to: currentDate) < maxAge
+            else {
+                return nil
+            }
+            return cachedResponse
+        } else {
+            return cachedResponse
+        }
+    }
+
+    func cache(output: Output, for request: URLRequest) {
+        let cachedResponse = CachedURLResponse(
+            response: output.response,
+            data: output.data,
+            userInfo: [Constant.cacheDateKey: Date()],
+            storagePolicy: .allowed
+        )
+        urlCache?.storeCachedResponse(cachedResponse, for: request)
     }
 
     func dataTaskPublisher(forRequestTo path: String) -> AnyPublisher<Data, ServiceError> {
-        guard let baseURL = baseURL else {
-            return Fail(error: ServiceError.badBaseURL).eraseToAnyPublisher()
+        guard let baseURL = URL(string: Constant.baseURL) else {
+            return Fail(error: ServiceError.badBaseURL(Constant.baseURL))
+                .eraseToAnyPublisher()
         }
         let request = URLRequest(url: baseURL.appendingPathComponent("remote-jobs"))
-        return URLSession.shared.dataTaskPublisher(for: request)
+
+        if let cachedResponse = cachedResponse(for: request, maxAge: Constant.cacheMaxAge) {
+            return Just(cachedResponse.data)
+                .setFailureType(to: ServiceError.self)
+                .eraseToAnyPublisher()
+        }
+
+        return session.dataTaskPublisher(for: request)
             .subscribe(on: requestQueue)
-            .map { $0.data }
+            .tryMap { try Self.validate(output: $0) }
+            .tryCatch { [weak self] error -> DataTaskPublisher in
+                guard let cachedResponse = self?.cachedResponse(for: request) else {
+                    throw error
+                }
+                return Just((cachedResponse.data, cachedResponse.response))
+                    .setFailureType(to: ServiceError.self)
+                    .eraseToAnyPublisher()
+            }
             .mapError { ServiceError(error: $0) }
+            .handleOutput { [weak self] in self?.cache(output: $0, for: request) }
+            .map { $0.data }
             .eraseToAnyPublisher()
     }
+
 }
 
 // MARK: - APIServiceType
@@ -40,27 +121,28 @@ extension APIService: APIServiceType {
 
     func getJobs() -> JobsPublisher {
         dataTaskPublisher(forRequestTo: "remote-jobs")
-            .flatMap { Self.decode(type: JobsResponseModel.self, from: $0) }
+            .tryMap { try Self.decode(type: JobsResponseModel.self, from: $0) }
             .map { $0.jobs.map { Job(apiModel: $0) } }
-            .mapError { $0 }
             .eraseToAnyPublisher()
     }
 
 }
 
-// MARK: - ServiceError
+// MARK: - Constants
 
-extension APIService {
+private extension APIService {
 
-    enum ServiceError: Error {
+    enum Constant {
 
-        case badBaseURL
-        case urlError(URLError)
-        case decoding(Error)
+        static let responsesCachePath = "Responses"
+        static let cacheDateKey = "cache-date"
+        static let baseURL = "https://remotive.io/api"
 
-        init(error: URLError) {
-            self = .urlError(error)
-        }
+        /// 4 Hours.
+        static let cacheMaxAge: TimeInterval = 14_400
+
+        /// 100 MB.
+        static let capacity = 104_857_600
 
     }
 
